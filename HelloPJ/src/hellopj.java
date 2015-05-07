5a8140c36f6606e96a72271b4efb7c4ec721d5ca
misako27696 <ikedamisato@172.31.10.107>
2015-05-07 13:02:43 +0900

    commit

---
+++ HelloPJ/src/hellopj.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author ikedamisato
+ *
+ */
+public class hellopj {
+
+	/**
+	 * 
+	 */
+	public hellopj() {
+		// TODO 自動生成されたコンストラクター・スタブ
+		
+		System.out.println("出たよ");
+		
+	}
+
+}
